d1ee0ea5ec74fc22f243be70b23f12dbc9111478
“ayesha-24” <ayesharahim24@gmail.com>
2022-09-26 12:29:27 +0100

    configure server to handle post requests

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
-const beasts = require("./beasts")
+const beasts = require("./beasts");
 
 // Make a basic server
 const app = express();
+// Allow requests from other origins
+app.use(cors());
+// Tell Express to always read the body of POST requests
+app.use(express.json());
 
 // Set up the server routes
 app.get("/", (req, res) => {
@@ -23,5 +28,17 @@ app.get("/beasts/:id", (req, res) => {
     const filtered = beasts.filter(b => b.id == req.params.id);
     res.send(filtered[0]);
 });
+
+app.post("/beasts", (req, res) => {
+    //Grab the beast data
+    const newBeast = req.body;
+
+    //Add to the list of beasts
+
+
+    //Return a message saying it worked
+    res.send(newBeast)
+
+})
 
 module.exports = app;
